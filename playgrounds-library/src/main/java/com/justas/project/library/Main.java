5dd8d57e9ee485dfb603a78bcf491f14fbbb0b1c
Justas Subatavicius <justassub@gmail.com>
2020-02-06 20:50:57 +0200

    Innitial commit with project modules

---
+++ playgrounds-library/src/main/java/com/justas/project/library/Main.java
@@ -0,0 +1,6 @@
+package com.justas.project.library;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ playgrounds-server/src/main/java/com/justas/project/server/PlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.justas.project.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundApplication.class, args);
+    }
+}
