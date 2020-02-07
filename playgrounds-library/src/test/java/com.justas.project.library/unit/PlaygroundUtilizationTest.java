ce926db3548507be478374818158b903bce610e4
Justas Subatavicius <justassub@gmail.com>
2020-02-07 16:04:40 +0200

    Feat-3-implemented: calculate utilization

---
+++ playgrounds-library/src/test/java/com.justas.project.library/unit/PlaygroundUtilizationTest.java
@@ -0,0 +1,103 @@
+package com.justas.project.library.unit;
+
+import com.justas.project.library.model.Child;
+import com.justas.project.library.model.playground.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
+
+import static java.util.Arrays.asList;
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class PlaygroundUtilizationTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return asList(new Object[][]{
+                {
+                        new DoubleSwings(), 0, 0.0
+                },
+                {
+                        new DoubleSwings(), 1, 0.0
+                },
+                {
+                        new DoubleSwings(), 2, 100.0
+                },
+                {
+                        new DoubleSwings(), 4, 100.0
+                },
+                {
+                        new Slide(), 0, 0.0
+                },
+                {
+                        new Slide(), 1, 100.0
+                },
+                {
+                        new Slide(), 4, 100.0
+                },
+                {
+                        new Carousel(0), 0, 100.0
+                },
+                {
+                        new Carousel(1), 1, 100.0
+                },
+                {
+                        new Carousel(5), 4, 80.0
+                },
+                {
+                        new Carousel(9), 2, 22.22
+                },
+                {
+                        new BallPit(0), 0, 100.0
+                },
+                {
+                        new BallPit(1), 1, 100.0
+                },
+                {
+                        new BallPit(5), 4, 80.0
+                },
+                {
+                        new BallPit(9), 2, 22.22
+                }
+        });
+    }
+
+    @Parameter
+    public Playground playground;
+
+    @Parameter(1)
+    public int childrenToAdd;
+
+    @Parameter(2)
+    public double expectedUtility;
+
+
+    private Supplier<Child> generateChild = () -> Child.builder()
+            .name(randomUUID().toString())
+            .age(10)
+            .ticketId(1)
+            .build();
+
+
+    private IntConsumer addChildren = (n) -> {
+        for (int i = 0; i < n; i++) {
+            playground.addChildIntoPlayground(generateChild.get());
+        }
+    };
+
+    @Test
+    public void testPlaygroundUtilization() {
+        addChildren.accept(childrenToAdd);
+        assertThat(playground.calculateUtilization()).isEqualTo(expectedUtility);
+    }
+
+
+}
